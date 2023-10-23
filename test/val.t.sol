3cd0c7ccc1e78309ba8012509bbbb401b7a94a4f
wchh <wangchh@gmail.com>
2023-10-23 09:26:19 +0800

    add val.t.sol

--- test/val.t.sol
+++ test/val.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {Val} from "../src/val.sol";
 
-contract CounterTest is Test {
+contract ValTest is Test {
     Val public val;
 
     function setUp() public {
