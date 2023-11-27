6cb5ee8abe60517249ba180ee1e9f6bdd179d41c
w <wangchh@gmail.com>
2023-11-27 09:26:38 +0800

    fix token.sol

--- src/token.sol
+++ src/token.sol
@@ -11,18 +11,25 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // new version of Ownable constructor must provide the owner address, so use abstract contract to avoid compilation error
 abstract contract OFTWithFee is BaseOFTWithFee, ERC20 {
-
     uint internal immutable ld2sdRate;
 
-    constructor(string memory _name, string memory _symbol, uint8 _sharedDecimals, address _lzEndpoint) ERC20(_name, _symbol) BaseOFTWithFee(_sharedDecimals, _lzEndpoint) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _sharedDecimals,
+        address _lzEndpoint
+    ) ERC20(_name, _symbol) BaseOFTWithFee(_sharedDecimals, _lzEndpoint) {
         uint8 decimals = decimals();
-        require(_sharedDecimals <= decimals, "OFTWithFee: sharedDecimals must be <= decimals");
+        require(
+            _sharedDecimals <= decimals,
+            "OFTWithFee: sharedDecimals must be <= decimals"
+        );
         ld2sdRate = 10 ** (decimals - _sharedDecimals);
     }
 
     /************************************************************************
-    * public functions
-    ************************************************************************/
+     * public functions
+     ************************************************************************/
     function circulatingSupply() public view virtual override returns (uint) {
         return totalSupply();
     }
@@ -32,24 +39,38 @@ abstract contract OFTWithFee is BaseOFTWithFee, ERC20 {
     }
 
     /************************************************************************
-    * internal functions
-    ************************************************************************/
-    function _debitFrom(address _from, uint16, bytes32, uint _amount) internal virtual override returns (uint) {
+     * internal functions
+     ************************************************************************/
+    function _debitFrom(
+        address _from,
+        uint16,
+        bytes32,
+        uint _amount
+    ) internal virtual override returns (uint) {
         address spender = _msgSender();
         if (_from != spender) _spendAllowance(_from, spender, _amount);
         _burn(_from, _amount);
         return _amount;
     }
 
-    function _creditTo(uint16, address _toAddress, uint _amount) internal virtual override returns (uint) {
+    function _creditTo(
+        uint16,
+        address _toAddress,
+        uint _amount
+    ) internal virtual override returns (uint) {
         _mint(_toAddress, _amount);
         return _amount;
     }
 
-    function _transferFrom(address _from, address _to, uint _amount) internal virtual override returns (uint) {
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint _amount
+    ) internal virtual override returns (uint) {
         address spender = _msgSender();
         // if transfer from this contract, no need to check allowance
-        if (_from != address(this) && _from != spender) _spendAllowance(_from, spender, _amount);
+        if (_from != address(this) && _from != spender)
+            _spendAllowance(_from, spender, _amount);
         _transfer(_from, _to, _amount);
         return _amount;
     }
@@ -84,9 +105,13 @@ contract TToken is OFTWithFee, Pausable {
     event FallbackWithdraw(address indexed to, uint256 amount);
     event DropFailedMessage(uint16 srcChainId, bytes srcAddress, uint64 nonce);
 
-    constructor(address _lzEndpoint) OFTWithFee("Tide Token", "TToken", 8, _lzEndpoint) Ownable(msg.sender) {}
+    constructor(
+        address _lzEndpoint,
+        string memory _name,
+        string memory _symbol
+    ) OFTWithFee(_name, _symbol, 8, _lzEndpoint) Ownable(msg.sender) {}
 
-    function decimals() public pure override returns (uint8){
+    function decimals() public pure override returns (uint8) {
         return 18;
     }
 
@@ -140,15 +165,15 @@ contract TToken is OFTWithFee, Pausable {
 
         uint256 receivedTokenAmount;
         uint256 lastReceivedTimestamp = chainIdToLastReceivedTimestamp[
-        _srcChainId
+            _srcChainId
         ];
         uint256 currTimestamp = block.timestamp;
         if ((currTimestamp / (1 days)) > (lastReceivedTimestamp / (1 days))) {
             receivedTokenAmount = amount;
         } else {
             receivedTokenAmount =
-            chainIdToReceivedTokenAmount[_srcChainId] +
-            amount;
+                chainIdToReceivedTokenAmount[_srcChainId] +
+                amount;
         }
 
         uint256 inboundCap = chainIdToInboundCap[_srcChainId];
